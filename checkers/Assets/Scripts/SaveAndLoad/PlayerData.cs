31d8a3e6fb2270f7500de77d29547073de1cd371
Kacper Duda <kacper.d.duda@gmail.com>
2019-10-20 23:27:47 +0200

    Update PlayerData.cs

--- checkers/Assets/Scripts/SaveAndLoad/PlayerData.cs
+++ checkers/Assets/Scripts/SaveAndLoad/PlayerData.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerData 
 {
+    
+    
     public int numberOfPlayers;
     public int[,] BoardState;
     public int[,] WinningPositions;
