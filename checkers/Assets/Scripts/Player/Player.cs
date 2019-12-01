4926f724ad6b579240f53c6848dbc452a5b366ee
Kacper Duda <kacper.d.duda@gmail.com>
2019-12-01 20:47:27 +0100

    Update Player.cs

--- checkers/Assets/Scripts/Player/Player.cs
+++ checkers/Assets/Scripts/Player/Player.cs
@@ -8,7 +8,6 @@ using Minimax;
 /// -ListOfPieces used for calculeting all possible moves for each player
 /// -WinningPositions- list that contains all winning positions for player
 ///</summary>
-
 public class Player : IPlayer
 {
    public List<TileScript> ListOfPieces = new List<TileScript>();   
